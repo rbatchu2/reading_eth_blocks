5addf1d205d205c7b461b3ee6c8a1c56374f5e21
Ronit Batchu <ronitbatchu@Ronits-MacBook-Pro.local>
2025-03-24 16:09:00 -0400

    syntax

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -77,13 +77,11 @@ def get_contract_values(contract, admin_address, owner_address):
 	https://testnet.bscscan.com/address/0xaA7CAaDA823300D18D3c43f65569a47e78220073
 	"""
 	default_admin_role = int.to_bytes(0, 32, byteorder="big")
-
-    onchain_root = contract.functions.merkleRoot().call()
-    has_role = contract.functions.hasRole(default_admin_role, admin_address).call()
-    prime = contract.functions.getPrimeByOwner(owner_address).call()
+	onchain_root = contract.functions.merkleRoot().call()
+	has_role = contract.functions.hasRole(default_admin_role, admin_address).call()
+	prime = contract.functions.getPrimeByOwner(owner_address).call()
 
     return onchain_root, has_role, prime
-
 
 """
 	This might be useful for testing (main is not run by the grader feel free to change 
