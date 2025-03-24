4573c5c3c55097fa4bd9ba11c2384cf048fefbed
Ronit Batchu <ronitbatchu@Ronits-MacBook-Pro.local>
2025-03-24 00:04:30 -0400

    fix

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -16,22 +16,21 @@ def connect_to_eth():
 	return w3
 
 def connect_with_middleware(contract_json):
-	with open(contract_json, "r") as f:
-		d = json.load(f)
-		d = d['bsc']
-		address = d['address']
-		abi = d['abi']
+    with open(contract_json, "r") as f:
+        d = json.load(f)
+        d = d['bsc']
+        address = d['address']
+        abi = d['abi']
 
-	# TODO complete this method
-	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	w3 = 0
+    bnb_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+    w3 = Web3(HTTPProvider(bnb_url))
+    assert w3.is_connected(), f"Failed to connect to provider at {bnb_url}"
 
-	# The second section requires you to inject middleware into your w3 object and
-	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
-	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-	contract = 0
+    w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
 
-	return w3, contract
+    contract = w3.eth.contract(address=address, abi=abi)
+
+    return w3, contract
 
 def is_ordered_block(w3, block_num):
     """
